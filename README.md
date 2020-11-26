6abb6437a22d2dda3cec2f1dc412686fdff6090a
Aakash Sunil <axs200020@utdallas.edu>
2020-11-26 09:08:20 +0530

    Main Application and Readme to execute Code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CS6360_Database_Design
+
+To run the program (Assumption - Java is set up in the path)
+
+Through Command Prompt
+
+1. Open command prompt at the project location where the main application (Application.java) is present.
+2. Run javac Application.java to compile the code.
+3. Run java Application to run the complied code.
+
+Through VSCode
+
+1. Press F5 to run the code and open debug mode.
---
+++ src/app/Application.java
@@ -0,0 +1,26 @@
+package app;
+import java.util.Scanner;
+
+import utils.Commands;
+import utils.Settings;
+import utils.Statements;
+
+public class Application {
+    
+    public static void main(String args[]) {
+        Statements.splashScreen();
+        String userCommand = "";
+        Scanner scanner = new Scanner(System.in).useDelimiter(";");
+
+        while (!Settings.isExit()) {
+            System.out.print(Settings.getPrompt());
+            userCommand = scanner.next().replace("\n", " ").replace("\r", "").trim().toLowerCase();
+            Commands.parseUserCommand(userCommand);
+        }
+        System.out.println("Exiting.....");
+        System.out.println(Statements.EXIT);
+        scanner.close();
+    }
+
+    
+}
