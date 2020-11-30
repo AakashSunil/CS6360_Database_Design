6a22ad6207f079536148dc04f1df0e552bff3027
Yichao Li <yclissetj@gmail.com>
2020-11-30 17:53:41 -0600

    finsh basic functions in Storage part
    1. implements create, drop, insert, delete, update and query method
    2. can only handle equalities conditions now

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,25 +1,48 @@
 package app;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import utils.Commands;
-import utils.Settings;
-import utils.Statements;
+import Storage.Database;
 
 public class Application {
     
     public static void main(String args[]) {
-        Statements.splashScreen();
-        String userCommand = "";
-        Scanner scanner = new Scanner(System.in).useDelimiter(";");
+        Database database = new Database();
+        database.initial();
+        database.createTable("Student",
+                new ArrayList<>(Arrays.asList("name", "age", "gender")));
+        database.insertRowIntoTable("Student",
+                new ArrayList<>(Arrays.asList("John", "24", "male")));
+        database.insertRowIntoTable("Student",
+                new ArrayList<>(Arrays.asList("Taylor", "23", "male")));
+        database.insertRowIntoTable("Student",
+                new ArrayList<>(Arrays.asList("Alice", "23", "female")));
+        database.deleteRowsFromTable("Student", "age", "24");
+        database.updateRowsInTable("Student", "age", "25",
+                "name", "Taylor");
 
-        while (!Settings.isExit()) {
-            System.out.print(Settings.getPrompt());
-            userCommand = scanner.next().replace("\n", " ").replace("\r", "").trim().toLowerCase();
-            Commands.parseUserCommand(userCommand);
-        }
-        System.out.println("Exiting.....");
-        System.out.println(Statements.EXIT);
-        scanner.close();
+        ArrayList<String> tableNameList = database.showTables();
+        ArrayList<ArrayList<String>> queryResult = database.queryInTable(
+                "Student",
+                new ArrayList<>(Arrays.asList("gender", "name")),
+                "gender",
+                "male"
+        );
+
+        database.exit();
+
+//        Statements.splashScreen();
+//        String userCommand = "";
+//        Scanner scanner = new Scanner(System.in).useDelimiter(";");
+//
+//        while (!Settings.isExit()) {
+//            System.out.print(Settings.getPrompt());
+//            userCommand = scanner.next().replace("\n", " ").replace("\r", "").trim().toLowerCase();
+//            Commands.parseUserCommand(userCommand);
+//        }
+//        System.out.println("Exiting.....");
+//        System.out.println(Statements.EXIT);
+//        scanner.close();
     }
 
     
